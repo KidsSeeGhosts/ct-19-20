865f9dc47f60aaddcb5091f5fa59d5f97d6b9210
Ruairi O'Hare <ruairiohare@Ruairis-MacBook-Pro.local>
2019-11-04 21:37:47 +0000

    Tower of hanoi and function calls work!!!!

--- myMips.asm
+++ myMips.asm
@@ -1,64 +1,140 @@
 .data
+stringLabel1: .asciiz "Move disk 1 from rod"
+stringLabel2: .asciiz "to"
+stringLabel3: .asciiz "Move disk"
+stringLabel4: .asciiz " from "
+stringLabel5: .asciiz "to "
 
 
 .text
 
 j main
-	bar:
+	towerOfHanoi:
 move $fp $sp
 addi $sp,$sp,-4
 addi $sp,$sp,-4
+addi $sp,$sp,-4
+addi $sp,$sp,-4
 la $t8, 0($fp)
 lw $t8, ($t8)
-li $v0, 1
-move $a0, $t8
+li $s7 1
+beq $t8, $s7, EqualTo1
+li $s6, 0
+j AfterEqualTo1
+EqualTo1: 
+li $s6, 1
+AfterEqualTo1:
+beq $s6, 0, AfterIf1
+la $s7, stringLabel1
+li $v0, 4
+move $a0, $s7
 syscall
-la $t8, -4($fp)
-lw $t8, ($t8)
-li $v0, 1
-move $a0, $t8
-syscall
-la $t8, 0($fp)
-lw $t8, ($t8)
 la $s7, -4($fp)
 lw $s7, ($s7)
-add $s6, $t8, $s7
-move $t9, $s6
-j barEnd
-barEnd:
-move $sp, $fp
-jr $ra
-	main:
-move $fp $sp
-addi $sp,$sp,-4
-addi $sp,$sp,-4
-la $s6, 0($fp)
-li $s7 5
-sw $s7, ($s6)
-la $s7, -4($fp)
-li $t8 8
-sw $t8, ($s7)
-la $t8, -4($fp)
+li $v0, 11
+move $a0, $s7
+syscall
+la $s7, stringLabel2
+li $v0, 4
+move $a0, $s7
+syscall
+la $s7, -8($fp)
+lw $s7, ($s7)
+li $v0, 11
+move $a0, $s7
+syscall
+j towerOfHanoiEnd
+j AfterIfElse1
+AfterIf1:
+AfterIfElse1:
 addi $sp,$sp,-4
 sw $fp, ($sp)
 addi $sp,$sp,-4
 sw $ra, ($sp)
+la $s7, 0($fp)
+lw $s7, ($s7)
+li $t8 1
+sub $s5, $s7, $t8
+addi $sp,$sp,-4
+sw $s5, ($sp)
+la $t8, -4($fp)
+lw $t8, ($t8)
+addi $sp,$sp,-4
+sw $t8, ($sp)
+la $s7, -12($fp)
+lw $s7, ($s7)
+addi $sp,$sp,-4
+sw $s7, ($sp)
+la $s4, -8($fp)
+lw $s4, ($s4)
+addi $sp,$sp,-4
+sw $s4, ($sp)
+add $sp, $sp, 12
+jal towerOfHanoi
+add $sp, $sp, 4
+lw $ra ,($sp)
+addiu $sp,$sp,4
+lw $fp ,($sp)
+addiu $sp,$sp,4
+la $s4, stringLabel3
+li $v0, 4
+move $a0, $s4
+syscall
+la $s4, 0($fp)
+lw $s4, ($s4)
+li $v0, 1
+move $a0, $s4
+syscall
+la $s4, stringLabel4
+li $v0, 4
+move $a0, $s4
+syscall
+la $s4, -4($fp)
+lw $s4, ($s4)
+li $v0, 11
+move $a0, $s4
+syscall
+la $s4, stringLabel5
+li $v0, 4
+move $a0, $s4
+syscall
+la $s4, -8($fp)
+lw $s4, ($s4)
+li $v0, 11
+move $a0, $s4
+syscall
+addi $sp,$sp,-4
+sw $fp, ($sp)
+addi $sp,$sp,-4
+sw $ra, ($sp)
+addi $sp,$sp,-4
+sw $s5, ($sp)
 addi $sp,$sp,-4
 sw $s6, ($sp)
 addi $sp,$sp,-4
 sw $s7, ($sp)
 addi $sp,$sp,-4
 sw $t8, ($sp)
-la $s4, 0($fp)
-lw $s4, ($s4)
+la $s3, 0($fp)
+lw $s3, ($s3)
+li $s2 1
+sub $s1, $s3, $s2
 addi $sp,$sp,-4
-sw $s4, ($sp)
-la $s3, -4($fp)
+sw $s1, ($sp)
+la $s2, -12($fp)
+lw $s2, ($s2)
+addi $sp,$sp,-4
+sw $s2, ($sp)
+la $s3, -8($fp)
 lw $s3, ($s3)
 addi $sp,$sp,-4
 sw $s3, ($sp)
-add $sp, $sp, 4
-jal bar
+la $s0, -4($fp)
+lw $s0, ($s0)
+addi $sp,$sp,-4
+sw $s0, ($sp)
+add $sp, $sp, 12
+jal towerOfHanoi
 add $sp, $sp, 4
 lw $t8 ,($sp)
 addiu $sp,$sp,4
@@ -66,16 +142,84 @@ lw $s7 ,($sp)
 addiu $sp,$sp,4
 lw $s6 ,($sp)
 addiu $sp,$sp,4
+lw $s5 ,($sp)
+addiu $sp,$sp,4
 lw $ra ,($sp)
 addiu $sp,$sp,4
 lw $fp ,($sp)
 addiu $sp,$sp,4
-sw $t9, ($t8)
-la $t9, -4($fp)
-lw $t9, ($t9)
-li $v0, 1
-move $a0, $t9
-syscall
+towerOfHanoiEnd:
+move $sp, $fp
+jr $ra
+	main:
+move $fp $sp
+addi $sp,$sp,-4
+la $s0, 0($fp)
+li $t7 4
+sw $t7, ($s0)
+addi $sp,$sp,-4
+sw $fp, ($sp)
+addi $sp,$sp,-4
+sw $ra, ($sp)
+addi $sp,$sp,-4
+sw $s0, ($sp)
+addi $sp,$sp,-4
+sw $s1, ($sp)
+addi $sp,$sp,-4
+sw $s2, ($sp)
+addi $sp,$sp,-4
+sw $s3, ($sp)
+addi $sp,$sp,-4
+sw $s4, ($sp)
+addi $sp,$sp,-4
+sw $s5, ($sp)
+addi $sp,$sp,-4
+sw $s6, ($sp)
+addi $sp,$sp,-4
+sw $s7, ($sp)
+addi $sp,$sp,-4
+sw $t8, ($sp)
+la $t6, 0($fp)
+lw $t6, ($t6)
+addi $sp,$sp,-4
+sw $t6, ($sp)
+li $t5 'A'
+addi $sp,$sp,-4
+sw $t5, ($sp)
+li $t4 'C'
+addi $sp,$sp,-4
+sw $t4, ($sp)
+li $t3 'B'
+addi $sp,$sp,-4
+sw $t3, ($sp)
+add $sp, $sp, 12
+jal towerOfHanoi
+add $sp, $sp, 4
+lw $t8 ,($sp)
+addiu $sp,$sp,4
+lw $s7 ,($sp)
+addiu $sp,$sp,4
+lw $s6 ,($sp)
+addiu $sp,$sp,4
+lw $s5 ,($sp)
+addiu $sp,$sp,4
+lw $s4 ,($sp)
+addiu $sp,$sp,4
+lw $s3 ,($sp)
+addiu $sp,$sp,4
+lw $s2 ,($sp)
+addiu $sp,$sp,4
+lw $s1 ,($sp)
+addiu $sp,$sp,4
+lw $s0 ,($sp)
+addiu $sp,$sp,4
+lw $ra ,($sp)
+addiu $sp,$sp,4
+lw $fp ,($sp)
+addiu $sp,$sp,4
+li $t3 0
+move $t9, $t3
+j mainEnd
 mainEnd:
 move $sp, $fp
 li $v0, 10
